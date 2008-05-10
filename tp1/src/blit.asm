34466abfe6fe90251b5d9dd0126d04b395e396b4
matiaslopez <matiaslopez@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2008-05-10 06:23:18 +0000

    Esbozo de blit. Está casi andando, falta ver porque no detecta el fucsia.
    Eso es todo por hoy.
    Saludos. 
    Matías.
    
    "Que fácil es morir en una plaza,
    que simple es disparar desde tu casa"
    
    Tirano - No te va gustar

--- tp1/src/blit.asm
+++ tp1/src/blit.asm
@@ -1,9 +1,15 @@
 ; int blit(char *image, int w, int h)
 
-global blit
+%define imagen  [ebp+8]
+%define w       [ebp+12]
+%define h       [ebp+16]
 
-extern malloc
-extern free
+%define fucsia 		0xFF00FF00
+%define hacerRGB 	0xFFFFFF00
+%define borrarFucsia	0x000000FF
+%define azul		0x0096FF00
+
+global blit
 
 section .text
 
@@ -15,8 +21,52 @@ blit:
     push edi
     push esi
 
-; Nucleo de la funcion
+	mov esi, imagen
+	
+	;calculo la basura del ancho
+        mov eax, w
+        mov ecx, 0x3
+        mul ecx
 
+        mov edi, eax ; en edi tengo el tam de la imagen en bytes
+
+        mov ecx, 0x4
+        div ecx
+
+        add edi, edx    ; le agrego los bytes basura ;en edx queda la basura
+
+
+	;Reemplazo
+
+		mov edi, h
+cicloExterno
+		mov ecx, w
+
+cicloInterno:
+		mov eax,[esi]
+		and eax, hacerRGB
+		cmp eax, fucsia
+		jz reemplazar
+
+	regresar:
+		add esi, 0x3
+		dec ecx
+		jnz cicloInterno
+
+		add esi, edx ; agrego la basura
+		dec edi
+		jnz	cicloExterno
+		jmp fin
+
+reemplazar:
+		mov eax, [esi] ;Capaz podía hacer 2 copias a registro y no traer todo 2 veces.
+		and eax, borrarFucsia
+		or eax, azul
+		mov [esi], eax
+	
+		jmp regresar
+
+fin:
     pop esi
     pop edi
     pop ebx
