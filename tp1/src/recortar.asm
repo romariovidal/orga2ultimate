47dcb8579c52606325751a9ccbf2f73e7f62543e
matiaslopez <matiaslopez@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2008-05-10 05:51:36 +0000

    Como uno es groso y simplemente se copo....
    Una menos, ahora está recorta. ¡Mario ya camina!
    
    Saludos.
    Matías
    
    "Mañanas de Sol,
    bajo por el ascensor"
     
    Mañana en el Abasto - Sumo

--- tp1/src/recortar.asm
+++ tp1/src/recortar.asm
@@ -1,9 +1,18 @@
 ; int recortar(char *imagen, int paso, int wP, int w, int h, char *res)
 
-global recortar
+%define imagen 	[ebp+8]
+%define paso 	[ebp+12]
+%define wP		[ebp+16]
+%define w		[ebp+20]
+%define h		[ebp+24]
+%define res		[ebp+28]
 
-extern malloc
-extern free
+;Variables locales
+%define basuraAnchoSprite	[ebp-4]
+%define anchoImgBytes		[ebp-8]
+%define comienzoFilaCopia 	[ebp-12]
+
+global recortar
 
 section .text
 
@@ -11,15 +20,86 @@ section .text
 recortar:
     push ebp
     mov ebp, esp
+	sub esp, 12
     push ebx
     push edi
     push esi
 
-; Nucleo de la funcion
+	; esi tendrá el puntero a la imagen.
+	; ebx a res
+	mov esi, imagen 
+
+	; busco el tam de cada sprite en bytes
+		mov eax, wP
+		mov ecx, 0x3
+		mul ecx
+
+		mov edi, eax ; en edi tengo el tam de la imagen en bytes
+
+		mov ecx, 0x4
+		div ecx
+
+		add edi, edx	; le agrego los bytes basura
+		mov basuraAnchoSprite, edx
+	;Ya obtuve el tamaño
+
+	;Idem pero para la imagen
+		mov eax, w	
+		mov ecx, 0x3
+		mul ecx
+
+		mov ebx, eax ; en edi tengo el tam de la imagen en bytes
+
+		mov ecx, 0x4
+		div ecx
+
+		add ebx, edx	; le agrego los bytes basura
+;		mov ebx, eax
+		mov anchoImgBytes, ebx
+	;Ya obtuve el tamaño
+
+
+	;Obtengo el lugar donde empieza el sprite buscado
+	mov ecx, paso
+	cmp ecx, 0
+	jz posicionado
+	
+
+avanzarDeSprite:
+	add esi, edi
+	dec ecx
+	jnz avanzarDeSprite
+	;Ya estoy parado al comienzo de mi sprite
+		
+posicionado:
+	mov ebx, res
+	mov edi, h
+
+cicloExterno:
+    mov ecx, wP   ;ecx contiene el ancho de la imagen en pixeles
+	mov comienzoFilaCopia, esi
+
+cicloInterno:
+    mov eax, [esi]  ;traigo el dibujo de memoria    
+    mov [ebx], eax
+
+    add esi, 0x3
+    add ebx, 0x3
+    dec ecx
+
+    jnz cicloInterno ;sigo la linea
+	
+	add ebx, basuraAnchoSprite
+	mov esi, comienzoFilaCopia
+	add esi, anchoImgBytes
+	dec edi
+	jnz cicloExterno
+	
 
     pop esi
     pop edi
     pop ebx
+	add esp, 12
     pop ebp
 
     ret
