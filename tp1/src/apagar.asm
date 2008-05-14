3c45c0fdea06c4d8186b52aa942d7d2dca8160bb
horizontedesucesos <horizontedesucesos@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2008-05-14 23:50:22 +0000

    =no esta terminada, da segFault, pero la sigo mirando en casa

--- tp1/src/apagar.asm
+++ tp1/src/apagar.asm
@@ -5,21 +5,65 @@ global apagar
 extern malloc
 extern free
 
+%define image 		[ebp+8]
+%define w 		[ebp+12]
+%define h 		[ebp+16]
+%define contador 	[ebp+20]
+
 section .text
 
-
 apagar:
-    push ebp
-    mov ebp, esp
-    push ebx
-    push edi
-    push esi
+	push ebp
+	mov ebp, esp
+	push ebx
+	push edi
+	push esi
+	
+	; Principio Nucleo de la funcion
+	mov edi, image 	; puntero a char (apunta a la imagen)
+	mov ecx, w	; contiene el ancho del sprite
+	mov ebx, h	; contiene el alto del sprite
+	mov eax, contador 
+	mov esi, [eax]
+	
+	
+ciclo_fila:
+	mov eax, [edi]
+	and eax, 0xfff0
+	cmp eax, 0xf0f0 
+	je  pasarDePixel
+	mov byte [edi], 0
+	inc edi
+	mov byte al, [esi]
+	mov byte [edi], al
+	inc edi
+	mov byte [edi], al
+	inc edi
+	loop ciclo_fila
 
-; Nucleo de la funcion
+	jmp pasarDeFila
 
-    pop esi
-    pop edi
-    pop ebx
-    pop ebp
+pasarDePixel:
+	add edi, 3
+	loop ciclo_fila
 
-    ret
+pasarDeFila:
+	dec ebx
+	cmp ebx, 0
+	je  fin
+	mov ecx, w
+	jmp ciclo_fila
+
+fin:
+	mov eax, [esi]
+	sub eax, 8
+	mov ebx, contador
+	mov [ebx], eax
+	; Fin Nucleo de la funcion
+	
+	pop esi
+	pop edi
+	pop ebx
+	pop ebp
+	
+	ret
