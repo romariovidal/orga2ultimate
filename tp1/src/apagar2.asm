d8569531fe54036721d9529d3826e5c24daec808
matiaslopez <matiaslopez@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2008-05-16 16:42:58 +0000

    Mando otras modificaciones, va cambiando de color, pero hay que ajustarlo un poco. Me voy a Luján. Hablamos más tarde o a la noche.
    Saludos.
    Matías.

--- tp1/src/apagar2.asm
+++ tp1/src/apagar2.asm
@@ -8,12 +8,17 @@
 %define fucsia 		0xFF00FF00
 %define hacerRGB 	0xFFFFFF00
 %define borrarFucsia	0x00000000
-%define azul		0x0096FF00
+%define borrarRojo		0x00FFFFFF
+
+extern printf
 
 global apagar
 
-section .text
+section .data
+formato db "Llamado con (%d, %d, %d, %d)",10,0
 
+
+section .text
 
 apagar:
     push ebp
@@ -26,6 +31,20 @@ apagar:
 	dec esi		; Esta trampa todavía no la entendí
 ;	inc esi		; Esta trampa todavía no la entendí
 ;	inc esi		; Esta trampa todavía no la entendí
+	mov ecx, w
+	mov ebx, h
+	mov edi, pCont
+	mov edi, [edi]
+
+	push edi
+	push ebx
+	push ecx
+	push esi 
+	push formato
+	call printf
+	add esp, 20
+
+
 	
 	;calculo la basura del ancho
         mov eax, w
@@ -65,15 +84,33 @@ cicloInterno:
 		jmp fin
 
 reemplazar:
-;		mov eax, [esi] ;Capaz podía hacer 2 copias a registro y no traer todo 2 veces.
-;		and eax, borrarFucsia
 		;or eax, azul
-		and eax, borrarFucsia
+		;nd eax, borrarFucsia
+		;PREPARANDO LA RESTA
+		mov eax, pCont
+		mov dword eax, [eax]
+		and eax, 0x000000FF
+		mov ebx, eax
+		shl eax,16
+		shl ebx, 8
+		or ebx, eax
+
+		mov eax, [esi] ;Capaz podía hacer 2 copias a registro y no traer todo 2 veces.
+		and eax, borrarRojo
+	
+		;RESTANDO EL GREEN Y EL BLUE
+		sub eax, ebx
+		
 		mov [esi], eax
 	
 		jmp regresar
 
 fin:
+	mov esi, pCont
+	mov eax, [esi]
+	sub eax, 8
+	mov [esi], eax
+
     pop esi
     pop edi
     pop ebx
