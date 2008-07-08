48b1a00df99f192ab8d685f4f7a4903d78de40bf
gnigel <gnigel@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2008-07-08 04:17:39 +0000

    ya esta apagar

--- tp2/src/apagar.asm
+++ tp2/src/apagar.asm
@@ -5,17 +5,12 @@
 %define h 		[ebp+16]
 %define pCont 		[ebp+20]
 
-%define fucsia 		0xFF00FF00
-%define hacerRGB 	0xFFFFFF00
-%define borrarFucsia	0x00000000
-%define borrarRojo	0x00FFFFFF
-
-extern printf
+;extern printf
 
 global apagar
 
 section .data
-formato db "Llamado con (%d)",10,0
+;formato db "Llamado con (%d)",10,0
 mascaraFucsia db 0x00, 0xFF, 0x00, 0xFF, 0xFF, 0x00, 0xFF, 0x00
 fucsiaCorrido db 0x00, 0xFF, 0x00, 0xFF
 
@@ -30,6 +25,7 @@ apagar:
 	push esi
 
 	mov esi, sprite
+
 	mov ecx, w
 	mov edi, ecx
 	
@@ -48,17 +44,20 @@ apagar:
 	sub edx, ecx	; calculo la basura, 4 - resto
 	
 edxTieneLaBasura:
-;	movd mm6, edx	; mm6 tiene la basura
 
 	mov eax, pCont
 	mov eax, [eax]
+
+	pxor mm7, mm7	
 	movd mm7, eax
 	pshufw mm7, mm7, 00000000b	; mm7 = 0,d,0,d,0,d,0,d
-	psllw mm7, 8
-	movd mm7, eax
+	psllq mm7, 8
+	pxor mm6, mm6	
+	movd mm6, eax
+	por mm7, mm6
 	pshufw mm7, mm7, 00001111b	; mm7 = 0,d,0,d,d,0,d,0
-	psrlq mm7, 8
-	pshufw mm7, mm7, 11010010b	; mm7 = 0,0,d,d,0,d,d,0
+	psrlq mm7, 8			; mm7 = 0,0,d,0,d,d,0,d
+	pshufw mm7, mm7, 00100111b	; mm7 = 0,d,d,0,d,d,0,0
 
 	movd mm5, [fucsiaCorrido]	; mm5 tiene la mascara para 1 pixel
 	movq mm6, [mascaraFucsia]	; mm6 tiene la mascara
@@ -76,31 +75,17 @@ cicloInterno:
 	psllq mm1, 16
 	psrlq mm1, 8	; dejo los 2 pixel al medio
 	movq mm4, mm1
-;	psubd mm1, mm6
-;	pxor mm2, mm2
-;	pcmpeqd mm2, mm1	; 0 si fucsia, 1 si reemplazar
 	pcmpeqd mm1, mm6
 
-	pxor mm3, mm3
-	pcmpeqd mm3, mm1
+	pand mm4, mm1
+	pandn mm1, mm7
+	por mm1, mm4
 
-	pand mm3, mm7
-	pand mm1, mm4
-	por mm1, mm3
-
-
-	
-;	movq mm1, mm6
-;	pand mm1, mm2	; 0 si no reemplazar, reemplazado
-;	pxor mm2, mm2
-;	pcmpeqd mm2, mm1	; 1 si no reemplazar, 0 si reemplazado
-;	pand mm2, mm4	; valor anterior si no reemplazar, 0 si reemplazar
-;	por mm1, mm2	; valor a guardar en el medio
-	
 	psrlq mm1, 8
 	psrlq mm0, 48
 	psllq mm0, 48
 	por mm0, mm1
+
 	movq [esi], mm0	; guarde lo que corresponde
 
 	add esi, 6
@@ -116,9 +101,9 @@ cicloUltimos:
 	dec esi	; esto es para no traerme memoria que no me corresponde
 	mov edx, [esi]
 	movd ebx, mm5	; traigo la mascara
-	mov dl, bl	; copio el byte que no me corresponde en la parte menos significativa
+	mov bl, dl	; copio el byte que no me corresponde en la parte menos significativa
 	sub ebx, edx
-	jz reemplazar
+	jnz reemplazar
 
 finReemplazar:
 	add esi, 4
@@ -146,8 +131,15 @@ fin:
 	mov esi, pCont
 	mov eax, [esi]
 	sub eax, 8
+	jle ponerCero
+	mov [esi], eax
+	jmp termine
+
+ponerCero:
+	xor eax, eax
 	mov [esi], eax
 
+termine:
 	pop esi
 	pop edi
 	pop ebx
