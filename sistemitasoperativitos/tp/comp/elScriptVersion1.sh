56f5d9df4a0a5ec12208cc0426f25cbf56c3b2fd
horizontedesucesos <horizontedesucesos@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2008-11-15 17:07:10 +0000

    allal

--- sistemitasoperativitos/tp/comp/elScriptVersion1.sh
+++ sistemitasoperativitos/tp/comp/elScriptVersion1.sh
@@ -1,45 +1,65 @@
 #!/bin/bash
 
-echo "Hola puto"
+#echo "Hola puto"
+#echo $1, $2, $3
 
+#if(($# == 3))
+#then
+#	echo "(3) Son $# par'ametros"
+#	if (( $3 == "-r" ))
+#	then
+#		echo "ser'a recursivo"
+#	fi
+#fi
 
-if(($# == 3))
-then
-	echo "(3) Son $# par'ametros"
-	if (( $3 == "-r" ))
-	then
-		echo "ser'a recursivo"
-	fi
-fi
+#echo "Carpeta origen : $1"
+#echo "Carpeta destino : $2"
 
-echo "Carpeta origen : $1"
-echo "Carpeta destino : $2"
+ORIGEN=$1
+DESTINO=$2
 
-ORIGEN="/home/tomas/comp"
-DESTINO="/home/tomas/local"
-
-echo "Listado de archivos: "
+#echo "Listado de archivos: "
 
 for file in $(ls $ORIGEN)
 do
-	echo "Procesando archivo: $file"
+#	echo "Procesando archivo: $file"
 	#echo "fecha de modificacion: $(stat -c %Y $file)"
-	FORIGEN=$(stat -c %Y $file)
+	FORIGEN=$(stat -c %Y $ORIGEN/$file)
 	ARCHIVODESTINO="$DESTINO/$file"
+	ARCHIVOORIGEN="$ORIGEN/$file"
 	#echo "Comparar contra $ARCHIVODESTINO"
 
-	#if [ -d "$ARCHIVODESTINO" ] para verificar si es directorio
+	if [ -d "$ARCHIVOORIGEN" ] 
+	#ARCHIVOORIGEN es un directorio
+	then
+		if [ -d "$ARCHIVODESTINO" ] 
+		then
+			if (( $3 == "-r" ))
+			then
+#				echo "caso recursivo"
+				sh elScriptVersion1.sh $ARCHIVOORIGEN $ARCHIVODESTINO $3
+			fi
+		else
+#			echo "file $ARCHIVOORIGEN"
+			cp -r $ARCHIVOORIGEN $DESTINO
+		fi
+			
+	#else
+	fi
+	
 	if [ -e "$ARCHIVODESTINO" ]
 	then
-		echo "-- Existe el archivo de destino, verificar"
+		#echo "-- Existe el archivo de destino, verificar"
 		FDESTIN=$(stat -c %Y $DESTINO/$file)
 		if [ "$FORIGEN" -gt "$FDESTIN" ]
 		then
-			echo "---- El origen es m'as nuevo, actualizar"
-		else
-			echo "---- El destino es m'as nuevo, ignorar"
+			#echo "---- El origen es mas nuevo, actualizar: $file"			
+			cp $ORIGEN/$file $DESTINO
+			#else
+			#echo "---- El destino es mas nuevo, ignorar"
 		fi
 	else
-		echo "-- Copiar como nuevo"
+		#echo "copio archivo que no esta en el destino: $file"
+		cp $ORIGEN/$file $DESTINO
 	fi
 done 
