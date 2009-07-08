9fbc665af80937816e6f17af064d2d8e3a9fc5aa
matiaslopez@gmail.com <matiaslopez@gmail.com@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-07-08 00:10:19 +0000

    pipe Version 1.0

---
+++ sistemitasoperativitos/tp2009/grupo1/pipeReentrega.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+//#include <cstdlib.h>
+
+int randomNumber() {
+	int value=(rand()%3+1);
+	
+	return value;
+}
+
+int main(void){
+        int     fd[2], res;
+        pid_t   pidDelHijo;
+        char    mensaje[] = "TomA mate";
+		int 	longMsg = 9;
+		int 	i = 0;
+        char    loMensajeado[80];
+
+        pipe(fd);
+      
+		printf("Bla\n"); 
+
+		perror("BLA");
+		while(res = write(fd[1],mensaje, 1) != 1);
+			if (res == -1) perror("primer signal");
+				else printf("ok\n");
+
+		printf("BLA2");
+	   	if((pidDelHijo = fork()) == -1)        {
+                perror("fork");
+                exit(1);
+        }
+		//fork();
+		
+		while(1){			
+	        if(pidDelHijo != 0) 
+				printf("Soy el proceso A a punto de entrar al WAIT");
+			else
+				printf("Soy el proceso B a punto de entrar al WAIT");
+
+			while (res = read(fd[0], loMensajeado, 1) != 1)
+				if (res == -1) perror("wait");
+			// adentro de la exclusión mutua
+			i++;
+
+	        if(pidDelHijo != 0) 
+				printf("Soy el proceso A a punto de hacer SIGNAL");
+			else
+				printf("Soy el proceso B a punto de hacer SIGNAL");
+
+			while(res=write(fd[0], mensaje, 1) != 1)
+				if (res == -1) perror("signal");
+
+		}
+
+/*
+        if(pidDelHijo != 0)        {
+                close(fd[0]);
+                write(fd[1], mensaje, strlen(mensaje));
+				close(fd[1]);
+                exit(0);
+        }else{
+                close(fd[1]);
+                res = read(fd[0], loMensajeado, sizeof(loMensajeado));
+                printf("Mensaje recibido: %s\n", loMensajeado);
+				exit(0);
+        }*/
+        
+        return(0);
+}
+
+
