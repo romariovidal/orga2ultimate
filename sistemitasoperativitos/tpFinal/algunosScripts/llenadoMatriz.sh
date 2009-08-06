2a03835f014681e2cebcc36928a9ead50512586a
matiaslopez <matiaslopez@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-08-06 16:36:14 +0000

    bla

---
+++ sistemitasoperativitos/tpFinal/algunosScripts/llenadoMatriz.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "\nLista para la matriz"
+
+for i in `seq 1 8`
+do
+	for j in `seq 1 8`
+	do
+		echo "\t\tlaMatriz[$i][$j] = new Integer(text_tiene$i$j.getText());"
+	done
+done
+
+
