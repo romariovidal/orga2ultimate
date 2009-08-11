ed51a3e00f9752bb901668b8f694153223415536
horizontedesucesos <horizontedesucesos@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-08-11 20:21:56 +0000

    =

---
+++ sistemitasoperativitos/tpFinal/Banquero2/src/banquero2/BanqueroAlgorithm.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package banquero2;
+
+/**
+ *
+ * @author tomas
+ */
+public class BanqueroAlgorithm {
+
+    Vector finish;
+    Matriz necesidad;
+    Vector disponibles;
+    Matriz asignacion;
+    int i;
+
+    private int paso;
+
+    public BanqueroAlgorithm(){}
+}
