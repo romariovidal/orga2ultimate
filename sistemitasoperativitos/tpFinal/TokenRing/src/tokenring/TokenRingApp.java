1bc0d002cad2711e09237745e36c7d85d5bd4fba
horizontedesucesos <horizontedesucesos@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-08-13 22:30:04 +0000

    =ToquenRing

---
+++ sistemitasoperativitos/tpFinal/TokenRing/src/tokenring/TokenRingApp.java
@@ -0,0 +1,44 @@
+/*
+ * TokenRingApp.java
+ */
+
+package tokenring;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class TokenRingApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new TokenRingView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of TokenRingApp
+     */
+    public static TokenRingApp getApplication() {
+        return Application.getInstance(TokenRingApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(TokenRingApp.class, args);
+    }
+}
