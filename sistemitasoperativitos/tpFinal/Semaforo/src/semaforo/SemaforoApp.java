5efb25e4bcaed784522a2d6a054c6bc72e71e061
horizontedesucesos <horizontedesucesos@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-08-18 22:11:42 +0000

    =

---
+++ sistemitasoperativitos/tpFinal/Semaforo/src/semaforo/SemaforoApp.java
@@ -0,0 +1,44 @@
+/*
+ * SemaforoApp.java
+ */
+
+package semaforo;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class SemaforoApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new SemaforoView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of SemaforoApp
+     */
+    public static SemaforoApp getApplication() {
+        return Application.getInstance(SemaforoApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(SemaforoApp.class, args);
+    }
+}
