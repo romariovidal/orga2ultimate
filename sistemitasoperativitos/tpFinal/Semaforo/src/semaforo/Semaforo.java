62b3e646f2fa099df07ee186cea1cb3495418241
matiaslopez <matiaslopez@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-08-23 18:24:08 +0000

    Bla....

--- sistemitasoperativitos/tpFinal/Semaforo/src/semaforo/Semaforo.java
+++ sistemitasoperativitos/tpFinal/Semaforo/src/semaforo/Semaforo.java
@@ -5,6 +5,7 @@
 
 package semaforo;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,7 @@ import java.util.List;
  *
  * @author tomas
  */
-public class Semaforo {
+public class Semaforo implements Serializable {
 
     private Boolean esP;
     private Integer valor;
