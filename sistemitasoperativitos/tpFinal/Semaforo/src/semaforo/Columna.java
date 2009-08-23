47f5bddeceb5480592087a03f561d9dce9e6c180
horizontedesucesos <horizontedesucesos@08a4e246-6a4b-0410-95e3-ad9fcf55e1c4>
2009-08-23 22:26:52 +0000

    4 pe

--- sistemitasoperativitos/tpFinal/Semaforo/src/semaforo/Columna.java
+++ sistemitasoperativitos/tpFinal/Semaforo/src/semaforo/Columna.java
@@ -15,14 +15,20 @@ import java.util.List;
  */
 public class Columna implements Serializable {
 
-    private List<Integer>   colaInicial=new ArrayList<Integer>();
-    private List<Semaforo>  semaforosSuperiores= new ArrayList<Semaforo>();
-    private List<Integer>   procesosEnZonaCritica=new ArrayList<Integer>();
-    private List<Integer>   colaInferior=new ArrayList<Integer>();
-    private List<Semaforo>  semaforosInferiores= new ArrayList<Semaforo>();
-    private Integer         procesoID=1;
+    private List<Integer>   colaInicial = new ArrayList<Integer>();
+    private List<Semaforo>  semaforosSuperiores = new ArrayList<Semaforo>();
+    private List<Integer>   procesosEnZonaCritica = new ArrayList<Integer>();
+    private List<Integer>   colaInferior = new ArrayList<Integer>();
+    private List<Semaforo>  semaforosInferiores = new ArrayList<Semaforo>();
+    private Integer         procesoID = 1;
+    private List<Integer>   procesosTerminados = new ArrayList<Integer>();
 
     public Columna() {
+    }
+
+    public void vaciarColumnas(){
+        List<Integer> none = new ArrayList<Integer>();
+        this.colaInferior.retainAll(none);
     }
 
     public void agregarNuevoProcesoAColaInicial(){
@@ -50,6 +56,14 @@ public class Columna implements Serializable {
         return colaInicial;
     }
 
+    public List<Semaforo> getSemaforosInferiores() {
+        return semaforosInferiores;
+    }
+
+    public List<Semaforo> getSemaforosSuperiores() {
+        return semaforosSuperiores;
+    }
+
     public Integer cantSemaforosSup(){
         return this.semaforosSuperiores.size();
     }
@@ -61,7 +75,7 @@ public class Columna implements Serializable {
     public String mostrarSemaforoSup(Integer i){
         return this.semaforosSuperiores.get(i).mostrar();
     }
-    
+
     public String mostrarSemaforoInf(Integer i){
         return this.semaforosInferiores.get(i).mostrar();
     }
